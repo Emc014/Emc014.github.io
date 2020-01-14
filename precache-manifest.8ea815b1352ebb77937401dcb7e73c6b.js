ab9a1b8c80bc0a4b8b9c0476dc8a8396813250b2
Emc14 <Emc014@hotmail.com>
2020-01-14 11:05:51 -0300

    Primera vercion para GitHub

---
+++ precache-manifest.8ea815b1352ebb77937401dcb7e73c6b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ec56e70d84365618ce43071d05964bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8035900e090cee480429",
+    "url": "/static/css/main.84f4721f.chunk.css"
+  },
+  {
+    "revision": "afc897af646c8f4d9b78",
+    "url": "/static/js/2.5f7364b2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5f7364b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8035900e090cee480429",
+    "url": "/static/js/main.925b656f.chunk.js"
+  },
+  {
+    "revision": "5b11c5c8880274af2de0",
+    "url": "/static/js/runtime-main.e58b6ec2.js"
+  },
+  {
+    "revision": "668d9cec88b7631113a8dda887b6859d",
+    "url": "/static/media/Guada.668d9cec.png"
+  },
+  {
+    "revision": "3b4dceaca265d2bacd0c210d7cb60d38",
+    "url": "/static/media/Robota.3b4dceac.otf"
+  },
+  {
+    "revision": "0f28623ddffcbfe5415575b2db807f01",
+    "url": "/static/media/fondo.0f28623d.svg"
+  },
+  {
+    "revision": "8629f2cb8ec6d15d3b048e30ac3938f5",
+    "url": "/static/media/yo.8629f2cb.jpg"
+  }
+]);
