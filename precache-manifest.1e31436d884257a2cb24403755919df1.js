1ea03f218c7a5c851ee0e2d7f2d17dee57e4ac50
EMC14 <emc014@hotmail.com>
2020-01-20 05:14:32 -0300

    nueva identidad

---
+++ precache-manifest.1e31436d884257a2cb24403755919df1.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2246c3fb2e5500c7d2e0616e7cd69892",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba28c11e2345594648ba",
+    "url": "/static/css/main.0bcace08.chunk.css"
+  },
+  {
+    "revision": "704fec83051c521a6f95",
+    "url": "/static/js/2.db37e6f1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.db37e6f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba28c11e2345594648ba",
+    "url": "/static/js/main.51524b1e.chunk.js"
+  },
+  {
+    "revision": "5b11c5c8880274af2de0",
+    "url": "/static/js/runtime-main.e58b6ec2.js"
+  },
+  {
+    "revision": "76b5dcd023b733a5bf733f9caef21574",
+    "url": "/static/media/AWS.76b5dcd0.jpg"
+  },
+  {
+    "revision": "fd8ad9a7736ce0a22d6f2826765e800d",
+    "url": "/static/media/E-mail.fd8ad9a7.png"
+  },
+  {
+    "revision": "668d9cec88b7631113a8dda887b6859d",
+    "url": "/static/media/Guada.668d9cec.png"
+  },
+  {
+    "revision": "42258b6e6effaafdd759f3aec40f3407",
+    "url": "/static/media/JavaScript.42258b6e.jpg"
+  },
+  {
+    "revision": "a1f498383668b3039d6169623924a53c",
+    "url": "/static/media/Programacion.a1f49838.jpg"
+  },
+  {
+    "revision": "d7afc71eda8dcad30590fce305d1c3f3",
+    "url": "/static/media/React.d7afc71e.jpg"
+  },
+  {
+    "revision": "3b4dceaca265d2bacd0c210d7cb60d38",
+    "url": "/static/media/Robota.3b4dceac.otf"
+  },
+  {
+    "revision": "5dff9f4cea6461e4e9b34f87931e493a",
+    "url": "/static/media/Telefono.5dff9f4c.png"
+  },
+  {
+    "revision": "ee24c29da1545e390bda0b61f5f833ff",
+    "url": "/static/media/WhatApp.ee24c29d.png"
+  },
+  {
+    "revision": "4568d3d01f51a4a0e91296b81a58360a",
+    "url": "/static/media/WordPress.4568d3d0.jpg"
+  },
+  {
+    "revision": "0171030b661c321a75c2a71ce6e53db2",
+    "url": "/static/media/desarrolloWeb.0171030b.jpg"
+  },
+  {
+    "revision": "0f28623ddffcbfe5415575b2db807f01",
+    "url": "/static/media/fondo.0f28623d.svg"
+  },
+  {
+    "revision": "06ceeaeb03b086bbcae8dde48b6bc3b2",
+    "url": "/static/media/grid.06ceeaeb.jpg"
+  },
+  {
+    "revision": "94f4dd8d5d03ed691a5972f9ba096f0c",
+    "url": "/static/media/jquery.94f4dd8d.jpg"
+  },
+  {
+    "revision": "3fc1a258a572aea2d613fba557b90743",
+    "url": "/static/media/php.3fc1a258.jpg"
+  },
+  {
+    "revision": "8629f2cb8ec6d15d3b048e30ac3938f5",
+    "url": "/static/media/yo.8629f2cb.jpg"
+  }
+]);
